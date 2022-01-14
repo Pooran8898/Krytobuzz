95650cfebe13cd4d3b0b1acc691ffe7dc01bccb3
Pooran8898 <poorancoc@gmail.com>
2022-01-14 21:34:47 +0530

    Navbar Item Updated

---
+++ client/src/Components/Navbar/NavbarItem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const NavbarItem = ({ title, classProps }) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    )
+}
